5f711ca1e606a2097557767ce35df59aaec7858b
phuc <phuc.phung@parcelperform.com>
2025-01-16 16:33:12 +0700

    update code test matrix generators

--- application-sets/my-application-sets/run.sh
+++ application-sets/my-application-sets/run.sh
@@ -8,7 +8,7 @@ argocd app create my-application-sets \
 		--dest-name in-cluster \
     --dest-namespace default
 
-argocd login kubernetes-vm:30443 --insecure --username admin --password vC24TXyZNEy7BD3X
+argocd login kubernetes-vm:30443 --insecure --username admin --password 8YstmpVTT5laQbTv
 argocd cluster add default --name cluster-2
 argocd cluster add default --name cluster-3
 
